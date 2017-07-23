69135e26d5bc3c4e04495472153e9794bc2ab530
sters <sters@ua.fm>
2017-07-23 12:24:42 +0300

    Почав робити 29-й

---
+++ p0291_simpleactivityresult/src/main/java/ua/anif/sters/simpleactivityresult/InputActivity.java
@@ -0,0 +1,36 @@
+package ua.anif.sters.simpleactivityresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class InputActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnOk;
+    EditText etName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        btnOk = (Button) findViewById(R.id.btnOk);
+        etName = (EditText) findViewById(R.id.etName);
+        etName.setText("");
+    }
+
+    @Override
+    public void onClick(View view) {
+        String name = (String) etName.getText().toString();
+
+        if (name.equalsIgnoreCase("")) {
+            Toast.makeText(view.getContext(), "Не введено имя", Toast.LENGTH_LONG).show();
+        } else {
+            Intent intent = getIntent();
+        }
+    }
+}
---
+++ p0291_simpleactivityresult/src/main/java/ua/anif/sters/simpleactivityresult/MainActivity.java
@@ -0,0 +1,29 @@
+package ua.anif.sters.simpleactivityresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnOpenInput;
+    TextView tvName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvName = (TextView) findViewById(R.id.tvName);
+        btnOpenInput = (Button) findViewById(R.id.btnOpenInput);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), InputActivity.class);
+        startActivityForResult(intent, 1);
+    }
+}
