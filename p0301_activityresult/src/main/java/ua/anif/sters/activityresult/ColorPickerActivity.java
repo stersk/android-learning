045c8e17b50e375ec6832e903fc9cea5deaf72f9
sters <sters@ua.fm>
2017-07-27 10:30:58 +0300

    Чуть зробив 30-го

--- p0301_activityresult/src/main/java/ua/anif/sters/activityresult/ColorPickerActivity.java
+++ p0301_activityresult/src/main/java/ua/anif/sters/activityresult/ColorPickerActivity.java
@@ -1,13 +1,52 @@
 package ua.anif.sters.activityresult;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 
-public class ColorPickerActivity extends AppCompatActivity {
+public class ColorPickerActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnRed;
+    Button btnBlue;
+    Button btnGreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
+
+        btnRed = (Button) findViewById(R.id.btnColorRed);
+        btnBlue = (Button) findViewById(R.id.btnColorBlue);
+        btnGreen = (Button) findViewById(R.id.btnColorGreen);
+
+        btnRed.setOnClickListener(this);
+        btnBlue.setOnClickListener(this);
+        btnGreen.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = getIntent();
+        int color = 0;
+
+        switch (view.getId()) {
+            case R.id.btnColorRed:
+                color = Color.RED;
+                break;
+            case R.id.btnColorBlue:
+                color = Color.BLUE;
+                break;
+            case R.id.btnColorGreen:
+                color = Color.GREEN;
+                break;
+        }
+
+        intent.putExtra("color", color);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
