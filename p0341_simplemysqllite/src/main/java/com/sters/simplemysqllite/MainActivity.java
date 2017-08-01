e28cf6157ffc4f7bd0ca9951a9f25c90c147a124
Sters <sters@ua.fm>
2017-08-01 23:57:37 +0300

    Зробив 35-ту задачу

--- p0341_simplemysqllite/src/main/java/com/sters/simplemysqllite/MainActivity.java
+++ p0341_simplemysqllite/src/main/java/com/sters/simplemysqllite/MainActivity.java
@@ -11,17 +11,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     final private String LOG_TAG = "SimpleDB";
     final private String DB_NAME ="db";
 
-    Button btnAdd, btnClear, btnRead;
-    EditText etName, etMail;
+    Button btnAdd, btnClear, btnRead, btnSelect, btnRemove, btnUpdate;
+    EditText etName, etMail, etId;
 
     DBOpenHelper openDbHelper;
     SQLiteDatabase writebleDb;
+
+    String index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +35,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         etName = (EditText) findViewById(R.id.etName);
         etMail = (EditText) findViewById(R.id.etMail);
+        etId = (EditText) findViewById(R.id.etId);
 
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnRead = (Button) findViewById(R.id.btnRead);
         btnClear = (Button) findViewById(R.id.btnClear);
+        btnSelect = (Button) findViewById(R.id.btnSelect);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnRemove = (Button) findViewById(R.id.btnRemove);
 
         btnAdd.setOnClickListener(this);
         btnRead.setOnClickListener(this);
         btnClear.setOnClickListener(this);
+        btnSelect.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+        btnRemove.setOnClickListener(this);
     }
 
     @Override
@@ -84,9 +94,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                     do {
                         Log.d(LOG_TAG, "-------------------------------------------------------------");
-                        Log.d(LOG_TAG, " id - " + String.valueOf(dataCursor.getInt(indexId)) + "----");
-                        Log.d(LOG_TAG, " name - " + String.valueOf(dataCursor.getString(indexName)) + "----");
-                        Log.d(LOG_TAG, " e-mail - " + String.valueOf(dataCursor.getString(indexMail)) + "----");
+                        Log.d(LOG_TAG, " id - " + String.valueOf(dataCursor.getInt(indexId)) + " ----");
+                        Log.d(LOG_TAG, " name - " + String.valueOf(dataCursor.getString(indexName)) + " ----");
+                        Log.d(LOG_TAG, " e-mail - " + String.valueOf(dataCursor.getString(indexMail)) + " ----");
                     } while (
                         dataCursor.moveToNext()
                     );
@@ -98,6 +108,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 dataCursor.close();
 
+                break;
+
+            case R.id.btnSelect:
+
+                index = etId.getText().toString();
+                if (index.equalsIgnoreCase("")) {
+                    Toast.makeText(this, "Введено некорректный идентфикатор", Toast.LENGTH_LONG).show();
+                } else {
+                    writebleDb = openDbHelper.getReadableDatabase();
+                    Cursor result = writebleDb.query(true, DB_NAME, null, "id = ?", new String[]{index}, null, null, null, null, null);
+                    if (result.moveToFirst()) {
+                        int indexId = result.getColumnIndex("id");
+                        int indexName = result.getColumnIndex("name");
+                        int indexMail = result.getColumnIndex("email");
+
+                        Log.d(LOG_TAG, "-------------------------------------------------------------");
+                        Log.d(LOG_TAG, " id - " + String.valueOf(result.getInt(indexId)) + " ----");
+                        Log.d(LOG_TAG, " name - " + String.valueOf(result.getString(indexName)) + " ----");
+                        Log.d(LOG_TAG, " e-mail - " + String.valueOf(result.getString(indexMail)) + " ----");
+                        Log.d(LOG_TAG, "-------------------------------------------------------------");
+
+                        etName.setText(result.getString(indexName));
+                        etMail.setText(result.getString(indexMail));
+                    } else {
+                        Log.d(LOG_TAG, "-------------------------------------------------------------");
+                        Log.d(LOG_TAG, " Строки с id " + String.valueOf(index) + " не найдено ----");
+                        Log.d(LOG_TAG, "-------------------------------------------------------------");
+
+                        etName.setText("");
+                        etMail.setText("");
+                    }
+
+                    result.close();
+                }
+                break;
+
+            case R.id.btnUpdate:
+                index = etId.getText().toString();
+                if (index.equalsIgnoreCase("")) {
+                    Toast.makeText(this, "Введено некорректный идентфикатор", Toast.LENGTH_LONG).show();
+                } else {
+                    writebleDb = openDbHelper.getWritableDatabase();
+                    ContentValues values = new ContentValues();
+                    values.put("id", index);
+                    values.put("name", etName.getText().toString());
+                    values.put("email", etMail.getText().toString());
+
+                    writebleDb.update(DB_NAME, values, "id = ?", new String[]{index});
+                    writebleDb.close();
+                    Log.d(LOG_TAG, " Данные строки с id " + String.valueOf(index) + " перезаписаны ----");
+                }
+                break;
+
+            case R.id.btnRemove:
+                index = etId.getText().toString();
+                if (index.equalsIgnoreCase("")) {
+                    Toast.makeText(this, "Введено некорректный идентфикатор", Toast.LENGTH_LONG).show();
+                } else {
+                    writebleDb = openDbHelper.getWritableDatabase();
+
+                    int rowsDeleted = writebleDb.delete(DB_NAME, "id = ?", new String[]{index});
+                    writebleDb.close();
+                    Log.d(LOG_TAG, " Удалено " + String.valueOf(rowsDeleted) + " строку(строк) ----");
+                }
+                break;
         }
         openDbHelper.close();
     }
