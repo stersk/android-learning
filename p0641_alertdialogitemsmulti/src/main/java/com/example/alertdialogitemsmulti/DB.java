2feb0f014d7354830696bfd04cb1eb7e462f78b4
sters <sters@ua.fm>
2017-08-27 17:07:05 +0300

    Кнопав 64-гий.

---
+++ p0641_alertdialogitemsmulti/src/main/java/com/example/alertdialogitemsmulti/DB.java
@@ -0,0 +1,73 @@
+package com.example.alertdialogitemsmulti;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DB {
+    private final Context context;
+    private final int dbVersion = 1;
+    private final String DB_NAME = "DialogItemSingle";
+    private final String MAIN_TABLE = "main";
+
+    private MySqliteDBOpenHelper dbOpenHelper;
+    private SQLiteDatabase db;
+
+    public DB(Context mContesxt) {
+        context = mContesxt;
+    }
+
+    public void open() {
+        dbOpenHelper = new MySqliteDBOpenHelper(context, DB_NAME, dbVersion);
+        db = dbOpenHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        if (dbOpenHelper != null) {
+            dbOpenHelper.close();
+        }
+    }
+
+    public Cursor getAllData() {
+        return db.query(true, MAIN_TABLE, null, null, null, null, null, null, null);
+    }
+
+    private class MySqliteDBOpenHelper extends SQLiteOpenHelper {
+
+        public MySqliteDBOpenHelper(Context context, String name, int version) {
+            super(context, name, null, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            sqLiteDatabase.execSQL("create table " + MAIN_TABLE + " (" +
+                    "_id integer primary key autoincrement," +
+                    "text text," +
+                    "checked boolean)");
+
+            ContentValues cv = new ContentValues();
+            cv.put("text", "Первая строка");
+            cv.put("checked", false);
+            sqLiteDatabase.insert(MAIN_TABLE, null, cv);
+            cv.clear();
+
+            cv.put("text", "Вторая строка");
+            cv.put("checked", false);
+            sqLiteDatabase.insert(MAIN_TABLE, null, cv);
+            cv.clear();
+
+            cv.put("text", "Третья строка");
+            cv.put("checked", false);
+            sqLiteDatabase.insert(MAIN_TABLE, null, cv);
+            cv.clear();
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        }
+    }
+}
---
+++ p0641_alertdialogitemsmulti/src/main/java/com/example/alertdialogitemsmulti/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.alertdialogitemsmulti;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DB myDB;
+    final int DATA_ITEM_SINGLE = 1;
+    Cursor cursor;
+    Context context;
+    int currentPosition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+        currentPosition = -1;
+
+        myDB = new DB(this);
+        myDB.open();
+
+        cursor = myDB.getAllData();
+    }
+
+    public void onclick(View view) {
+        showDialog(DATA_ITEM_SINGLE);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DATA_ITEM_SINGLE) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            dialogBuilder.setTitle("Выберите один из пунктов");
+            dialogBuilder.setMultiChoiceItems(cursor, "checked", "text", onMultiChoiceClickListener);
+            dialogBuilder.setPositiveButton("Ок", onItemSelected);
+            return dialogBuilder.create();
+        }
+
+        return super.onCreateDialog(id);
+    }
+
+    DialogInterface.OnClickListener onItemSelected = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            currentPosition = i;
+            int fieldIdx = cursor.getColumnIndex("text");
+            int fieldCheckedIdx = cursor.getColumnIndex("checked");
+
+            if (i == AlertDialog.BUTTON_POSITIVE) {
+                AlertDialog dialog = (AlertDialog) dialogInterface;
+                ListView listView = (ListView) dialog.getListView();
+                listView.getCheckedItemPositions();
+            }
+        }
+    };
+
+    DialogInterface.OnMultiChoiceClickListener onMultiChoiceClickListener = new DialogInterface.OnMultiChoiceClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+            cursor.moveToPosition(i);
+            int column = cursor.getColumnIndex("text");
+            String text = cursor.getString(column);
+
+            Toast.makeText(context, "Пункт " + text + " " + (b ? "ВЫБРАН" : "НЕ ВЫБРАН"), Toast.LENGTH_LONG).show();
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        cursor.close();
+        myDB.close();
+    }
+}
