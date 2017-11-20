ffebeaad863aee16d30f90e59b71c24872c4aa4f
sters <sters@ua.fm>
2017-11-21 00:18:07 +0200

    Добив 99

--- p0991_servicenotification/src/main/java/com/example/servicenotification/MyService.java
+++ p0991_servicenotification/src/main/java/com/example/servicenotification/MyService.java
@@ -1,14 +1,61 @@
 package com.example.servicenotification;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-
+import java.util.concurrent.TimeUnit;
 
 public class MyService extends Service {
     NotificationManager notificationManager;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        sendMessage();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void sendMessage() {
+
+        // 3-я часть
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(MainActivity.FILE_NAME, "somefile");
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        // 1-я и 2-я часть
+        // c API 24 уже не катит
+        //notif.setLatestEventInfo(this, "Notification's title", "Notification's text", pIntent);
+        Notification.Builder builder = new Notification.Builder(this);
+
+        builder.setAutoCancel(true);
+        builder.setTicker("Ticker text");
+        builder.setContentTitle("Notification's title");
+        builder.setContentText("Notification's text");
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        builder.setContentIntent(pIntent);
+        builder.setOngoing(true);
+        builder.setSubText("This is subtext...");   //API level 16
+        builder.setNumber(100);
+
+        Notification notif = builder.build();
+        // c API 24 уже не катит
+
+        // отправляем
+        notificationManager.notify(1, notif);
+    }
 
     @Override
     public void onCreate() {
@@ -19,7 +66,6 @@ public class MyService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 }
