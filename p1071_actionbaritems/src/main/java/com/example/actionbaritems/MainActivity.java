26101d42ed95842129c8a97dd66b2bbb9ddf6a77
sters <sters@ua.fm>
2018-01-24 22:06:35 +0200

    Зробив 107

---
+++ p1071_actionbaritems/src/main/java/com/example/actionbaritems/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.actionbaritems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        return true;
+    }
+}
