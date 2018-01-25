3088c406d959940162a1d0bb36b09cb40338f8bc
sters <sters@ua.fm>
2018-01-25 22:59:33 +0200

    Зробив 109

---
+++ p1091_listfragment/src/main/java/com/example/listfragment/MainList.java
@@ -0,0 +1,26 @@
+package com.example.listfragment;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class MainList extends ListFragment {
+    String data[] = new String[] { "one", "two", "three", "four" };
+    //String data[] = new String[] {};
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment, null);
+    }
+}
---
+++ p1092_listfragmentcheckbox/src/main/java/com/example/listfragmentcheckbox/MainList.java
@@ -0,0 +1,38 @@
+package com.example.listfragmentcheckbox;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainList extends ListFragment {
+    String data[] = new String[] { "one", "two", "three", "four" };
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ListView listView = getListView();
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        //listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_multiple_choice, data);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1, data);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment, null);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+            super.onListItemClick(l, v, position, id);
+            Toast.makeText(getActivity(), "position = " + position, Toast.LENGTH_SHORT).show();
+    }
+}
