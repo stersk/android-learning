e3f9cfc7dc59588b6433b8dc20bd47f0eb506920
sters <sters@ua.fm>
2018-02-27 23:27:47 +0200

    Добив 119.

--- p1191_pendingintent/src/main/java/com/example/pendingintent/MainActivity.java
+++ p1191_pendingintent/src/main/java/com/example/pendingintent/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,12 +32,14 @@ public class MainActivity extends AppCompatActivity {
         am = (AlarmManager) getSystemService(ALARM_SERVICE);
     }
 
-    public void onClick1(View view) {
-        intent1 = createIntent("action 1", "extra 1");
+   /* public void onClick1(View view) {
+        intent1 = createIntent("action", "extra 1");
         pIntent1 = PendingIntent.getBroadcast(this, 0, intent1, 0);
 
-        intent2 = createIntent("action 2", "extra 2");
-        pIntent2 = PendingIntent.getBroadcast(this, 0, intent2, 0);
+        intent2 = createIntent("action", "extra 2");
+        //pIntent2 = PendingIntent.getBroadcast(this, 0, intent2, 0);
+        //pIntent2 = PendingIntent.getBroadcast(this, 0, intent2, PendingIntent.FLAG_CANCEL_CURRENT);
+        pIntent2 = PendingIntent.getBroadcast(this, 0, intent2, PendingIntent.FLAG_UPDATE_CURRENT);
 
         compare();
 
@@ -44,8 +48,72 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick2(View view) {
+        intent1 = createIntent("action", "extra 1");
+        pIntent1 = PendingIntent.getBroadcast(this, 0, intent1, PendingIntent.FLAG_ONE_SHOT);
 
+        sendNotif(1, pIntent1);
+        sendNotif(2, pIntent1);
+    }*/
+
+    /*public void onClick1(View view) {
+        intent1 = createIntent("action", "extra 1");
+        pIntent1 = PendingIntent.getBroadcast(this, 0, intent1, 0);
+        Log.d(LOG_TAG, "pIntent1 created");
     }
+
+    public void onClick2(View view){
+        intent2 = createIntent("action", "extra 2");
+        pIntent2 = PendingIntent.getBroadcast(this, 0, intent2, PendingIntent.FLAG_NO_CREATE);
+        if (pIntent2 == null) Log.d(LOG_TAG, "pIntent2 is null");
+        else Log.d(LOG_TAG, "pIntent2 created");
+    }
+    */
+
+    /*public void onClick1(View view) {
+        intent1 = createIntent("action", "extra 1");
+        pIntent1 = PendingIntent.getBroadcast(this, 0, intent1, 0);
+        sendNotif(1, pIntent1);
+    }
+
+    public void onClick2(View view) {
+        pIntent1.cancel();
+        Log.d(LOG_TAG, "cancel pIntent1");
+    }*/
+
+    /*public void onClick1(View view) {
+        intent1 = createIntent("action", "extra 1");
+        Uri data1 = Uri.parse(intent1.toUri(Intent.URI_INTENT_SCHEME));
+        intent1.setData(data1);
+        pIntent1 = PendingIntent.getBroadcast(this, 0, intent1, 0);
+
+        intent2 = createIntent("action", "extra 2");
+        Uri data2 = Uri.parse(intent2.toUri(Intent.URI_INTENT_SCHEME));
+        intent2.setData(data2);
+        pIntent2 = PendingIntent.getBroadcast(this, 0, intent2, 0);
+
+        compare();
+
+        sendNotif(1, pIntent1);
+        sendNotif(2, pIntent2);
+    }*/
+
+    public void onClick1(View view) {
+        intent1 = createIntent("action 1", "extra 1");
+        pIntent1 = PendingIntent.getBroadcast(this, 0, intent1, 0);
+
+        intent2 = createIntent("action 2", "extra 2");
+        pIntent2 = PendingIntent.getBroadcast(this, 0, intent2, 0);
+
+        Log.d(LOG_TAG, "start");
+        am.set(AlarmManager.RTC, System.currentTimeMillis() + 4000, pIntent1);
+        am.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + 3000, 5000, pIntent2);
+    }
+
+    public void onClick2(View view) {
+        am.cancel(pIntent2);
+    }
+
 
     Intent createIntent(String action, String extra) {
         Intent intent = new Intent(this, MyReceiver.class);
