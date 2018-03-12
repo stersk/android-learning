cb5200e3f318ad457f6de569aed74dbf9fbe8eaa
sters <sters@ua.fm>
2018-03-12 22:32:21 +0200

    Добив 125.

--- p1251_viewpager/src/main/java/com/example/viewpager/PageFragment.java
+++ p1251_viewpager/src/main/java/com/example/viewpager/PageFragment.java
@@ -4,6 +4,7 @@ package com.example.viewpager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,13 @@ import android.widget.TextView;
 
 import java.util.Random;
 
+import static com.example.viewpager.MainActivity.TAG;
+
 public class PageFragment extends Fragment {
 
 
     static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+    static final String SAVE_PAGE_NUMBER = "save_page_number";
 
     int pageNumber;
     int backColor;
@@ -31,9 +35,17 @@ public class PageFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
+        Log.d(TAG, "onCreate: " + pageNumber);
 
         Random rnd = new Random();
         backColor = Color.argb(40, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+
+        int savedPageNumber = -1;
+        if (savedInstanceState != null) {
+            savedPageNumber = savedInstanceState.getInt(SAVE_PAGE_NUMBER);
+        }
+        Log.d(TAG, "savedPageNumber = " + savedPageNumber);
+
     }
 
     @Override
@@ -46,6 +58,19 @@ public class PageFragment extends Fragment {
         tvPage.setBackgroundColor(backColor);
 
         return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SAVE_PAGE_NUMBER, pageNumber);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy: " + pageNumber);
     }
 
 }
