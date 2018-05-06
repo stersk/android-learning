ca7429318174bf0e7623f468470b3e7bf93dacfc
sters <sters@ua.fm>
2018-05-06 12:23:01 +0300

    Почав 150.

--- p1491_canvastext/src/main/java/com/example/p1491_canvastext/MainActivity.java
+++ p1491_canvastext/src/main/java/com/example/p1491_canvastext/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawView2(this));
+        setContentView(new DrawView5(this));
     }
 
     class DrawView extends View {
@@ -108,6 +109,130 @@ public class MainActivity extends AppCompatActivity {
             // полоса лимита
             p.setColor(Color.GREEN);
             canvas.drawLine(50, 270, 50 + maxWidth, 270, p);
+
+        }
+    }
+
+    class DrawView3 extends View {
+
+        Paint p;
+        String text = "Test width text";
+        int fontSize = 60;
+        float y = 80;
+
+        public DrawView3(Context context) {
+            super(context);
+            p = new Paint(Paint.ANTI_ALIAS_FLAG);
+            p.setTextSize(fontSize);
+            p.setStyle(Paint.Style.STROKE);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawARGB(80, 102, 204, 255);
+
+            // обычный текст
+            canvas.translate(50, y);
+            canvas.drawText(text, 0, 0, p);
+
+            // моноширинный
+            canvas.translate(0, y);
+            p.setTypeface(Typeface.create(Typeface.MONOSPACE, Typeface.NORMAL));
+            canvas.drawText(text, 0, 0, p);
+
+            // с засечками
+            canvas.translate(0, y);
+            p.setTypeface(Typeface.create(Typeface.SERIF, Typeface.NORMAL));
+            canvas.drawText(text, 0, 0, p);
+
+            // обычный жирный
+            canvas.translate(0, y);
+            p.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+            canvas.drawText(text, 0, 0, p);
+
+            // обычный жирный курсивный
+            canvas.translate(0, y);
+            p.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD_ITALIC));
+            canvas.drawText(text, 0, 0, p);
+
+            // обычный курсивный
+            canvas.translate(0, y);
+            p.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.ITALIC));
+            canvas.drawText(text, 0, 0, p);
+
+        }
+    }
+
+    class DrawView4 extends View {
+
+        Paint p;
+        String text = "Test width text";
+        int fontSize = 60;
+        float y = 80;
+
+        public DrawView4(Context context) {
+            super(context);
+            p = new Paint(Paint.ANTI_ALIAS_FLAG);
+            p.setTextSize(fontSize);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawARGB(80, 102, 204, 255);
+
+            // обычный текст
+            canvas.translate(50, y);
+            canvas.drawText(text, 0, 0, p);
+
+            // растянутый
+            canvas.translate(0, y);
+            p.setTextScaleX(1.5f);
+            canvas.drawText(text, 0, 0, p);
+            p.setTextScaleX(1);
+
+            // наклоненный
+            canvas.translate(0, y);
+            p.setTextSkewX(0.5f);
+            canvas.drawText(text, 0, 0, p);
+            p.setTextSkewX(0);
+
+            // подчеркнутый
+            canvas.translate(0, y);
+            p.setUnderlineText(true);
+            canvas.drawText(text, 0, 0, p);
+            p.setUnderlineText(false);
+
+            // зачеркнутый
+            canvas.translate(0, y);
+            p.setStrikeThruText(true);
+            canvas.drawText(text, 0, 0, p);
+            p.setStrikeThruText(false);
+
+        }
+    }
+
+    class DrawView5 extends View {
+
+        Paint p;
+        String text = "Test text";
+        int fontSize = 100;
+        float pos[];
+
+        public DrawView5(Context context) {
+            super(context);
+            p = new Paint(Paint.ANTI_ALIAS_FLAG);
+            p.setTextSize(fontSize);
+
+            pos = new float[]{100, 300, 200, 150, 300, 500, 400, 300, 500,
+                    250, 600, 350, 700, 400, 800, 200, 900, 500};
+
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawARGB(80, 102, 204, 255);
+
+            canvas.drawPosText(text, pos, p);
 
         }
     }
